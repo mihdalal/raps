0e25083bd366f2bdd27a70645da6e3f7daf7dd52
Murtaza Dalal <mdalal@andrew.cmu.edu>
2022-05-14 13:45:17 -0400

    dependency adjustment for dm_control

--- d4rl/setup.py
+++ d4rl/setup.py
@@ -12,7 +12,7 @@ setup(
         "h5py",
         "termcolor",  # adept_envs dependency
         "click",  # adept_envs dependency
-        "dm_control @ git+https://github.com/deepmind/dm_control@main#egg=dm_control",
+        "dm_control==1.0.0",
         "mjrl @ git+https://github.com/aravindr93/mjrl@master#egg=mjrl",
     ],
     packages=find_packages(),
